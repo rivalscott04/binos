800c7f029590934589942997232e1da9bc42d798
rivalscott04 <rival.biasrori@outlook.com>
2024-11-18 00:06:32 +0800

    gosip model controller view hot bangets v2

--- app/Views/transaksi/pembinaan/new.php
+++ app/Views/transaksi/pembinaan/new.php
@@ -42,7 +42,7 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                <!-- diisi oleh form dinamis dengan jQuery -->
+                                <!-- Baris dinamis akan diisi oleh jQuery -->
                             </tbody>
                         </table>
                     </div>
@@ -56,5 +56,54 @@
         </div>
     </div>
 </section>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    $(document).ready(function () {
+        let no = 1;
+
+        // Tambahkan baris baru
+        $('#Barisbaru').click(function (e) {
+            e.preventDefault();
+            let newRow = `
+            <tr>
+                <td>${no++}</td>
+                <td><input type="text" class="form-control" name="data[kegiatan][]" required></td>
+                <td><input type="text" class="form-control" name="data[uraian_kegiatan][]" required></td>
+                <td><input type="text" class="form-control" name="data[rincian][]" required></td>
+                <td><input type="number" class="form-control" name="data[volume][]" min="0" required></td>
+                <td><input type="number" class="form-control" name="data[harga_satuan][]" min="0" required></td>
+                <td><input type="number" class="form-control" name="data[jumlah][]" readonly></td>
+                <td><button class="btn btn-danger btn-sm btn-block remove"><i class="fa fa-trash"></i></button></td>
+            </tr>`;
+            $('#tableLoop tbody').append(newRow);
+        });
+
+        // Hapus baris
+        $('#tableLoop').on('click', '.remove', function (e) {
+            e.preventDefault();
+            $(this).closest('tr').remove();
+            // Reset nomor pada kolom pertama
+            resetRowNumbers();
+        });
+
+        // Hitung jumlah otomatis
+        $('#tableLoop').on('input', '[name="data[volume][]"], [name="data[harga_satuan][]"]', function () {
+            let row = $(this).closest('tr');
+            let volume = parseFloat(row.find('[name="data[volume][]"]').val()) || 0;
+            let hargaSatuan = parseFloat(row.find('[name="data[harga_satuan][]"]').val()) || 0;
+            row.find('[name="data[jumlah][]"]').val(volume * hargaSatuan);
+        });
+
+        // Reset nomor urut tabel
+        function resetRowNumbers() {
+            let rows = $('#tableLoop tbody tr');
+            no = 1;
+            rows.each(function () {
+                $(this).find('td:first').text(no++);
+            });
+        }
+    });
+</script>
 
 <?= $this->endSection() ?>
