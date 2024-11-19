6b423c6b4e5f4046dbcc65222ea1c36c1c03516c
rivalscott04 <rival.biasrori@outlook.com>
2024-11-19 11:17:26 +0800

    ini perbaikan

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'https://apiisena.site/';
+    public string $baseURL = 'https://apiisena.site/binos/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
