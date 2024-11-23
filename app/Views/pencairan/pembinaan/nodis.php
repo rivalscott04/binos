00e36b8380274a38baf5610fb209a3aad757ba85
rivalscott04 <rival.biasrori@outlook.com>
2024-11-23 15:22:50 +0800

    dinamis bulan tahun dan tanggal nodis

--- app/Views/pencairan/pembinaan/nodis.php
+++ app/Views/pencairan/pembinaan/nodis.php
@@ -180,8 +180,8 @@
             </table>
 
             <p style="margin-top: 20px; font-size: 14px;">Sehubungan dengan telah dilaksanakan kegiatan Pembinaan bulan
-            <?= isset($isi[0]->tanggal) ? strftime('%B', strtotime($isi[0]->tanggal)) : 'kosong' ?>
-            ni kami mengajukan permohonan biaya pelaksanaannya sesuai dengan ketentuan yang berlaku. Apabila
+            <?= isset($isi[0]->tanggal) ? strftime('%B' '%Y', strtotime($isi[0]->tanggal)) : 'kosong' ?>
+            dengan ini kami mengajukan permohonan biaya pelaksanaannya sesuai dengan ketentuan yang berlaku. Apabila
                 Bapak Berkenan mohon untuk ditindaklanjuti :</p>
             <table class="table">
                 <tr class="tr">
